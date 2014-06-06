04ca53cc51962fd807ce850acb9374ca55793d16
Paul B. <paul@bonaud.fr>
2014-06-06 16:13:17 +0200

    Update setup.sh

--- build/setup.sh
+++ build/setup.sh
@@ -1,7 +1,24 @@
 #!/usr/bin/env sh
 
-# Start MySQL
+# If mysql db directory is empty
+if [ ! -f /var/lib/mysql/ibdata1 ]; then
+  
+  /usr/bin/mysql_install_db
+
+  /usr/bin/mysqld_safe > /dev/null 2>&1 &
+  /bin/sleep 2s
+
+  /usr/bin/mysqladmin -u root password 'root'
+  /usr/bin/mysqladmin -u root password 'root'
+  
+  /usr/bin/killall mysqld
+  /bin/sleep 2s
+
+fi
+
+# Start Mysql
 /usr/bin/mysqld_safe > /dev/null 2>&1 &
+/bin/sleep 2s
 
 # Create Database
 mysql -uroot -proot -e "CREATE DATABASE a_database"
@@ -11,3 +28,5 @@ mysql -uroot -proot -e "GRANT ALL PRIVILEGES ON *.* to 'a_user'@'%' IDENTIFIED B
 
 # Shutdown MySQL
 mysqladmin -uroot -proot shutdown
+
+/sbin/my_init
